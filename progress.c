52daaa377820a15b91f6aaff8f50f75037a8ae77
Jeff King <peff@peff.net>
2011-11-09 22:43:19 -0500

    progress: allow pure-throughput progress meters
    The progress code assumes we are counting something (usually
    objects), even if we are measuring throughput. This works
    for fetching packfiles, since they show us the object count
    alongside the throughput, like:
    
      Receiving objects:   2% (301/11968), 22.00 MiB | 10.97 MiB/s
    
    You can also tell the progress code you don't know how many
    items you have (by specifying a total of 0), and it looks
    like:
    
      Counting objects: 34957
    
    However, if you're fetching a single large item, you want
    throughput but you might not have a meaningful count. You
    can say you are getting item 0 or 1 out of 1 total, but then
    the percent meter is misleading:
    
      Downloading:   0% (0/1), 22.00 MiB | 10.97 MiB/s
    
    or
    
      Downloading: 100% (0/1), 22.00 MiB | 10.97 MiB/s
    
    Neither of those is accurate. You are probably somewhere
    between zero and 100 percent through the operation, but you
    don't know how far.
    
    Telling it you don't know how many items is even uglier:
    
      Downloading: 1, 22.00 MiB | 10.97 MiB/s
    
    Instead, this patch will omit the count entirely if you are
    on the zero-th item of an unknown number of items. It looks
    like:
    
      Downloading: 22.00 MiB | 10.97 MiB/s
    
    Signed-off-by: Jeff King <peff@peff.net>

--- progress.c
+++ progress.c
@@ -133,8 +133,10 @@ static void display(struct progress *progress, uint64_t n, const char *done)
 			show_update = 1;
 		}
 	} else if (progress_update) {
-		strbuf_reset(counters_sb);
-		strbuf_addf(counters_sb, "%"PRIuMAX"%s", (uintmax_t)n, tp);
+		if (n) {
+			strbuf_reset(counters_sb);
+			strbuf_addf(counters_sb, "%"PRIuMAX"%s", (uintmax_t)n, tp);
+		}
 		show_update = 1;
 	}
 
@@ -170,10 +172,11 @@ static void display(struct progress *progress, uint64_t n, const char *done)
 }
 
 static void throughput_string(struct strbuf *buf, uint64_t total,
-			      unsigned int rate)
+			      unsigned int rate, struct progress *p)
 {
 	strbuf_reset(buf);
-	strbuf_addstr(buf, ", ");
+	if (p->total || p->last_value > 0)
+		strbuf_addstr(buf, ", ");
 	strbuf_humanise_bytes(buf, total);
 	strbuf_addstr(buf, " | ");
 	strbuf_humanise_rate(buf, rate * 1024);
@@ -242,7 +245,7 @@ void display_throughput(struct progress *progress, uint64_t total)
 	tp->last_misecs[tp->idx] = misecs;
 	tp->idx = (tp->idx + 1) % TP_IDX_MAX;
 
-	throughput_string(&tp->display, total, rate);
+	throughput_string(&tp->display, total, rate, progress);
 	if (progress->last_value != -1 && progress_update)
 		display(progress, progress->last_value, NULL);
 }
@@ -330,7 +333,7 @@ static void force_last_update(struct progress *progress, const char *msg)
 		unsigned int misecs, rate;
 		misecs = ((now_ns - progress->start_ns) * 4398) >> 32;
 		rate = tp->curr_total / (misecs ? misecs : 1);
-		throughput_string(&tp->display, tp->curr_total, rate);
+		throughput_string(&tp->display, tp->curr_total, rate, progress);
 	}
 	progress_update = 1;
 	buf = xstrfmt(", %s.\n", msg);
