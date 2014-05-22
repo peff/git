aa5a2f9cd951e940fcd6fe7450bcbff9b26d2573
Jeff King <peff@peff.net>
2014-05-22 15:07:51 -0400

    ahead-behind: stop using revision parser to handle arguments
    We have to go through some contortions to get
    handle_revision_arg to parse "foo..bar", and then find the
    result in the pending objects array. Instead, let's just
    look for the ".." ourselves, and parse each side
    individually.
    
    This is way simpler, and fixes the problems mentioned by
    f70590ae3101733b3048d7e5415c3ef6bd939c43 (namely, that we
    show the wrong tip name in some cases, and that we fail to
    notice some bad arguments).

--- builtin/ahead-behind.c
+++ builtin/ahead-behind.c
@@ -12,52 +12,36 @@ static const char * const ahead_behind_usage[] = {
 	NULL
 };
 
-static struct object_array_entry *find_pending(struct object_array *array,
-					       int flags,
-					       int mask)
-{
-	int i;
-	for (i = 0; i < array->nr; i++) {
-		struct object_array_entry *ent = array->objects + i;
-		if ((ent->item->flags & mask) == flags)
-			return ent;
-	}
-	return NULL;
-}
-
 static struct commit *default_base = NULL;
 static const char *base_ref = "master";
 
-static void handle_arg(struct rev_info *revs, const char *revarg)
+static struct commit *get_commit(const char *str)
 {
-	struct object_array_entry *tip_ent, *base_ent;
+	struct commit *c = lookup_commit_reference_by_name(str);
+	if (!c)
+		die("bad revision '%s'", str);
+	return c;
+}
+
+static void handle_arg(char *arg)
+{
 	struct commit *tip, *base;
+	char *dotdot;
 	int ahead, behind;
 
-	if (handle_revision_arg(revarg, revs, 0, REVARG_CANNOT_BE_FILENAME))
-		die("bad revision '%s'", revarg);
-
-	base_ent = find_pending(&revs->pending, UNINTERESTING, UNINTERESTING);
-	if (base_ent)
-		base = lookup_commit_reference(the_repository, &base_ent->item->oid);
-	else if (default_base)
+	dotdot = strstr(arg, "..");
+	if (dotdot) {
+		*dotdot = '\0';
+		base = get_commit(dotdot + 2);
+	} else {
+		if (!default_base)
+			default_base = get_commit(base_ref);
 		base = default_base;
-	else
-		base = default_base = lookup_commit_reference_by_name(base_ref);
-	if (!base)
-		die("missing base for ahead-behind stats");
-
-	tip_ent = find_pending(&revs->pending, 0, UNINTERESTING);
-	if (!tip_ent)
-		tip_ent = base_ent;
-	tip = lookup_commit_reference(the_repository, &tip_ent->item->oid);
-	if (!tip)
-		die("failed to lookup commit ref %s", oid_to_hex(&tip_ent->item->oid));
+	}
+	tip = get_commit(arg);
 
 	revision_ahead_behind(tip, base, &ahead, &behind, AHEAD_BEHIND_FULL);
-	printf("%s %d %d\n", tip_ent->name, ahead, behind);
-
-	object_array_clear(&revs->pending);
+	printf("%s %d %d\n", arg, ahead, behind);
 }
 
 int cmd_ahead_behind(int argc, const char **argv, const char *prefix)
@@ -70,12 +54,8 @@ int cmd_ahead_behind(int argc, const char **argv, const char *prefix)
 		OPT_END()
 	};
 
-	struct rev_info revs;
-
 	argc = parse_options(argc, argv, NULL, ahead_behind_opts,
 			     ahead_behind_usage, 0);
-
-	init_revisions(&revs, NULL);
 
 	if (from_stdin) {
 		struct strbuf line = STRBUF_INIT;
@@ -84,15 +64,18 @@ int cmd_ahead_behind(int argc, const char **argv, const char *prefix)
 			if (!line.len)
 				break;
 
-			handle_arg(&revs, line.buf);
+			handle_arg(line.buf);
 		}
 
 		strbuf_release(&line);
 	} else {
 		int i;
 
-		for (i = 0; i < argc; ++i)
-			handle_arg(&revs, argv[i]);
+		for (i = 0; i < argc; ++i) {
+			char *arg = xstrdup(argv[i]);
+			handle_arg(arg);
+			free(arg);
+		}
 	}
 
 	return 0;
