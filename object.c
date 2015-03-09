5170343c5b54cd2bcb4232701db671fa5bca8ed8
Jeff King <peff@peff.net>
2015-03-09 18:04:20 -0400

    metapacks: add generations
    this probably need a version bump and some refactoring
    around the metapack->data stride length

--- object.c
+++ object.c
@@ -255,9 +255,9 @@ struct object *parse_object(const struct object_id *oid)
 
 	if (!save_commit_buffer &&
 	    (!obj || (obj && obj->type == OBJ_COMMIT))) {
-		uint32_t timestamp;
+		uint32_t timestamp, generation;
 		const unsigned char *tree, *p1, *p2;
-		if (!commit_metapack(sha1, &timestamp, &tree, &p1, &p2)) {
+		if (!commit_metapack(sha1, &timestamp, &generation, &tree, &p1, &p2)) {
 			struct commit *commit;
 			if (obj)
 				commit = (struct commit *)obj;
@@ -265,6 +265,7 @@ struct object *parse_object(const struct object_id *oid)
 				commit = lookup_commit(sha1);
 
 			commit->date = timestamp;
+			commit->generation = generation;
 			commit->tree = lookup_tree(tree);
 			commit_list_insert(lookup_commit(p1), &commit->parents);
 			if (!is_null_sha1(p2))
