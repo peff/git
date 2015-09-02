fcf67eaba5d993f63880714ee16ef058b5160a18
Jeff King <peff@peff.net>
2015-09-01 23:23:13 -0400

    xdiff: use st_* helper functions for allocations
    The xdiff code is not prepared to handle extremely large
    files. Usually this isn't a problem, as we treat large files
    as binaries, anyway. But this behavior can be overridden,
    and feeding, for example, a 1GB file to xdiff is not pretty.
    We may overflow size_t calculations and allocate too-small
    buffers.
    
    This patch tries to do the minimal amount to make xdiff
    safe, by using git-compat-util's overflow-checking size_t
    calculations. Doubtless there are other bugs if you send
    such large input to it, but at least we will avoid
    overflowing buffers in this case.
    
    Signed-off-by: Jeff King <peff@peff.net>

--- xdiff/xdiffi.c
+++ xdiff/xdiffi.c
@@ -337,7 +337,7 @@ int xdl_do_diff(mmfile_t *mf1, mmfile_t *mf2, xpparam_t const *xpp,
 	 * One is to store the forward path and one to store the backward path.
 	 */
 	ndiags = xe->xdf1.nreff + xe->xdf2.nreff + 3;
-	if (!XDL_ALLOC_ARRAY(kvd, 2 * ndiags + 2)) {
+	if (!XDL_ALLOC_ARRAY(kvd, st_mult(2 ,st_add(ndiags, 2)))) {
 
 		xdl_free_env(xe);
 		return -1;
