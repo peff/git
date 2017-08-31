d0e273d621d4c55779898324c7728c72f3f141cd
Jeff King <peff@peff.net>
2017-08-31 02:01:27 -0400

    sigchain: use sigaction
    Except on Windows, where it's just a stub.
    
    Signed-off-by: Jeff King <peff@peff.net>

--- sigchain.c
+++ sigchain.c
@@ -4,7 +4,7 @@
 #define SIGCHAIN_MAX_SIGNALS 32
 
 struct sigchain_signal {
-	sigchain_fun *old;
+	struct sigaction *old;
 	int n;
 	int alloc;
 };
@@ -16,27 +16,49 @@ static void check_signum(int sig)
 		BUG("signal out of range: %d", sig);
 }
 
+/*
+ * On platforms that have it, we can use sigaction. But otherwise we just fake
+ * it by calling signal() and totally ignoring the sa_flags or sa_mask fields.
+ */
+static int maybe_sigaction(int sig,
+			  struct sigaction *sa,
+			  struct sigaction *old)
+{
+#ifndef GIT_WINDOWS_NATIVE
+	return sigaction(sig, sa, old);
+#else
+	old->sa_handler = signal(sig, sa->sa_handler);
+	return old->sa_handler == SIG_ERR ? -1 : 0;
+#endif
+}
+
 int sigchain_push(int sig, sigchain_fun f)
 {
+	struct sigaction sa;
 	struct sigchain_signal *s = signals + sig;
 	check_signum(sig);
 
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = f;
+	sa.sa_flags = SA_RESTART;
+
 	ALLOC_GROW(s->old, s->n + 1, s->alloc);
-	s->old[s->n] = signal(sig, f);
-	if (s->old[s->n] == SIG_ERR)
+	if (maybe_sigaction(sig, &sa, &s->old[s->n]) < 0)
 		return -1;
+
 	s->n++;
 	return 0;
 }
 
 int sigchain_pop(int sig)
 {
+	struct sigaction dummy;
 	struct sigchain_signal *s = signals + sig;
 	check_signum(sig);
 	if (s->n < 1)
 		return 0;
 
-	if (signal(sig, s->old[s->n - 1]) == SIG_ERR)
+	if (maybe_sigaction(sig, &s->old[s->n - 1], &dummy) < 0)
 		return -1;
 	s->n--;
 	return 0;
