f1a916cad012373e9169fd09673836d880b8ffa3
Jeff King <peff@peff.net>
2017-08-31 02:08:13 -0400

    sigchain: mask out common signals
    Signed-off-by: Jeff King <peff@peff.net>

--- sigchain.c
+++ sigchain.c
@@ -32,7 +32,7 @@ static int maybe_sigaction(int sig,
 #endif
 }
 
-int sigchain_push(int sig, sigchain_fun f)
+static int sigchain_push_with_mask(int sig, sigchain_fun f, const sigset_t *mask)
 {
 	struct sigaction sa;
 	struct sigchain_signal *s = signals + sig;
@@ -41,6 +41,10 @@ int sigchain_push(int sig, sigchain_fun f)
 	memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = f;
 	sa.sa_flags = SA_RESTART;
+#ifndef GIT_WINDOWS_NATIVE
+	if (mask)
+		memcpy(&sa.sa_mask, mask, sizeof(*mask));
+#endif
 
 	ALLOC_GROW(s->old, s->n + 1, s->alloc);
 	if (maybe_sigaction(sig, &sa, &s->old[s->n]) < 0)
@@ -48,6 +52,11 @@ int sigchain_push(int sig, sigchain_fun f)
 
 	s->n++;
 	return 0;
+}
+
+int sigchain_push(int sig, sigchain_fun f)
+{
+	return sigchain_push_with_mask(sig, f, NULL);
 }
 
 int sigchain_pop(int sig)
@@ -66,11 +75,20 @@ int sigchain_pop(int sig)
 
 void sigchain_push_common(sigchain_fun f)
 {
-	sigchain_push(SIGINT, f);
-	sigchain_push(SIGHUP, f);
-	sigchain_push(SIGTERM, f);
-	sigchain_push(SIGQUIT, f);
-	sigchain_push(SIGPIPE, f);
+	sigset_t mask;
+
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGINT);
+	sigaddset(&mask, SIGHUP);
+	sigaddset(&mask, SIGTERM);
+	sigaddset(&mask, SIGQUIT);
+	sigaddset(&mask, SIGPIPE);
+
+	sigchain_push_with_mask(SIGINT, f, &mask);
+	sigchain_push_with_mask(SIGHUP, f, &mask);
+	sigchain_push_with_mask(SIGTERM, f, &mask);
+	sigchain_push_with_mask(SIGQUIT, f, &mask);
+	sigchain_push_with_mask(SIGPIPE, f, &mask);
 }
 
 void sigchain_pop_common(void)
