9cd4061ad358e83feb5ee5ad48f99d57a7ea5841
Jeff King <peff@peff.net>
2017-09-19 15:13:02 -0400

    fix up metapacks perf-test &&-linting

--- t/perf/p0000-metapacks.sh
+++ t/perf/p0000-metapacks.sh
@@ -7,7 +7,7 @@ test_description="Tests history walking performance with metapacks"
 test_perf_default_repo
 
 test_expect_success 'build metapacks' '
-        rm -vf .git/objects/pack/*.commits
+        rm -vf .git/objects/pack/*.commits &&
 	# old versions do not have metapacks at all
 	test_might_fail git metapack --all --commits
 '
