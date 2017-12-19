9a206cb5c3ef1fc3b7b0d1a3f111d87151ce646f
Jeff King <peff@peff.net>
2017-12-19 04:58:34 -0500

    unpack-objects: drop -r option
    It doesn't actually work.

--- builtin/unpack-objects.c
+++ builtin/unpack-objects.c
@@ -14,7 +14,7 @@
 #include "decorate.h"
 #include "fsck.h"
 
-static int dry_run, quiet, recover, has_errors, strict;
+static int dry_run, quiet, has_errors, strict;
 static const char unpack_usage[] = "git unpack-objects [-n] [-q] [-r] [--strict]";
 
 /* We always read in 4kB chunks. */
@@ -117,8 +117,6 @@ static void *get_data(unsigned long size)
 		if (ret != Z_OK) {
 			error("inflate returned %d", ret);
 			FREE_AND_NULL(buf);
-			if (!recover)
-				exit(1);
 			has_errors = 1;
 			break;
 		}
@@ -434,8 +432,6 @@ static void unpack_delta_entry(enum object_type type, unsigned long delta_size,
 	if (!base) {
 		error("failed to read delta-pack base object %s",
 		      oid_to_hex(&base_oid));
-		if (!recover)
-			exit(1);
 		has_errors = 1;
 		return;
 	}
@@ -480,8 +476,6 @@ static void unpack_one(unsigned nr)
 	default:
 		error("bad object type %d", type);
 		has_errors = 1;
-		if (recover)
-			return;
 		exit(1);
 	}
 }
@@ -537,7 +531,7 @@ int cmd_unpack_objects(int argc, const char **argv, const char *prefix)
 				continue;
 			}
 			if (!strcmp(arg, "-r")) {
-				recover = 1;
+				warning("option -r is deprecated and does nothing");
 				continue;
 			}
 			if (!strcmp(arg, "--strict")) {
