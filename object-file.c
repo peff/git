d749ff55c5f675837af6fec91ada23f7d52f24b0
Jeff King <peff@peff.net>
2017-12-19 05:58:53 -0500

    sha1_file: drop read_pack_header()
    There are no more callers.
    
    Signed-off-by: Jeff King <peff@peff.net>

--- object-file.c
+++ object-file.c
@@ -1023,21 +1023,6 @@ int index_path(struct index_state *istate, struct object_id *oid,
 	return rc;
 }
 
-int read_pack_header(int fd, struct pack_header *header)
-{
-	if (read_in_full(fd, header, sizeof(*header)) != sizeof(*header))
-		/* "eof before pack header was fully read" */
-		return PH_ERROR_EOF;
-
-	if (header->hdr_signature != htonl(PACK_SIGNATURE))
-		/* "protocol error (pack signature mismatch detected)" */
-		return PH_ERROR_PACK_SIGNATURE;
-	if (!pack_version_ok(header->hdr_version))
-		/* "protocol error (pack version unsupported)" */
-		return PH_ERROR_PROTOCOL;
-	return 0;
-}
-
 int for_each_file_in_obj_subdir(unsigned int subdir_nr,
 				struct strbuf *path,
 				const struct git_hash_algo *algop,
