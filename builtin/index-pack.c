948e489bf1d0f15d09c8c63928b3fdc9563b4688
Jeff King <peff@peff.net>
2017-12-19 04:42:02 -0500

    index-pack: allow verifying over --stdin
    This obviously skips verification of the index, but allows
    you to see if the pack itself is valid. This is similar to
    unpack-objects' --dry-run.
    
    Signed-off-by: Jeff King <peff@peff.net>

--- builtin/index-pack.c
+++ builtin/index-pack.c
@@ -1912,9 +1912,7 @@ int cmd_index_pack(int argc,
 							 &rev_index_name_buf);
 	}
 
-	if (verify) {
-		if (!index_name)
-			die(_("--verify with no packfile name given"));
+	if (verify && index_name) {
 		read_idx_option(&opts, index_name);
 		opts.flags |= WRITE_IDX_VERIFY | WRITE_IDX_STRICT;
 	}
