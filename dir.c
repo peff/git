94eacdb8df1a5873a6596f43e35fdef7f7b2c4d5
Jeff King <peff@peff.net>
2018-10-17 01:03:28 -0400

    dir: mark match_pathname unused
    Arguably this could just go away.

--- dir.c
+++ dir.c
@@ -1231,7 +1231,7 @@ int match_basename(const char *basename, int basenamelen,
 int match_pathname(const char *pathname, int pathlen,
 		   const char *base, int baselen,
 		   const char *pattern, int prefix, int patternlen,
-		   unsigned flags)
+		   unsigned UNUSED(flags))
 {
 	const char *name;
 	int namelen;
