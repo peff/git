b582c9168fd7996b2871b3ef453f559d14adeab6
Jeff King <peff@peff.net>
2018-10-17 18:03:56 -0400

    curl cb unused

--- http.c
+++ http.c
@@ -187,7 +187,7 @@ size_t fread_buffer(char *ptr, size_t eltsize, size_t nmemb, void *buffer_)
 }
 
 #ifndef NO_CURL_IOCTL
-curlioerr ioctl_buffer(CURL *handle, int cmd, void *clientp)
+curlioerr ioctl_buffer(CURL *UNUSED(handle), int cmd, void *clientp)
 {
 	struct buffer *buffer = clientp;
 
@@ -214,7 +214,8 @@ size_t fwrite_buffer(char *ptr, size_t eltsize, size_t nmemb, void *buffer_)
 	return nmemb;
 }
 
-size_t fwrite_null(char *ptr, size_t eltsize, size_t nmemb, void *strbuf)
+size_t fwrite_null(char *UNUSED(ptr), size_t UNUSED(eltsize), size_t nmemb,
+		   void *UNUSED(data))
 {
 	return nmemb;
 }
@@ -746,7 +747,9 @@ static void curl_dump_data(const char *text, unsigned char *ptr, size_t size)
 	strbuf_release(&out);
 }
 
-static int curl_trace(CURL *handle, curl_infotype type, char *data, size_t size, void *userp)
+static int curl_trace(CURL *UNUSED(handle), curl_infotype type,
+		      char *data, size_t size,
+		      void *UNUSED(userp))
 {
 	const char *text;
 	enum { NO_FILTER = 0, DO_FILTER = 1 };
