37c7480aeb2fe9ca59b38b6e648a32f25bf5f4ce
Jeff King <peff@peff.net>
2018-10-17 01:43:54 -0400

    traverse_commit_list cb unused

--- pack-bitmap.c
+++ pack-bitmap.c
@@ -473,7 +473,8 @@ static void show_object(struct object *object, const char *name, void *data_)
 	bitmap_set(data->base, bitmap_pos);
 }
 
-static void show_commit(struct commit *commit, void *data)
+static void show_commit(struct commit *UNUSED(commit),
+			void *UNUSED(data))
 {
 }
 
@@ -1323,7 +1324,8 @@ struct bitmap_test_data {
 	size_t seen;
 };
 
-static void test_show_object(struct object *object, const char *name,
+static void test_show_object(struct object *object,
+			     const char *UNUSED(name),
 			     void *data)
 {
 	struct bitmap_test_data *tdata = data;
