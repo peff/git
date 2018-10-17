54af4feaddbf9944c00b95f66ac31aa4633bfba2
Jeff King <peff@peff.net>
2018-10-17 03:24:49 -0400

    trivial hash functions mark unused

--- sha1-file.c
+++ sha1-file.c
@@ -89,7 +89,6 @@ static void git_hash_sha1_final(unsigned char *hash, git_hash_ctx *ctx)
 	git_SHA1_Final(hash, &ctx->sha1);
 }
 
-
 static void git_hash_sha256_init(git_hash_ctx *ctx)
 {
 	git_SHA256_Init(&ctx->sha256);
@@ -110,22 +109,26 @@ static void git_hash_sha256_final(unsigned char *hash, git_hash_ctx *ctx)
 	git_SHA256_Final(hash, &ctx->sha256);
 }
 
-static void git_hash_unknown_init(git_hash_ctx *ctx)
+static void git_hash_unknown_init(git_hash_ctx *UNUSED(ctx))
 {
 	BUG("trying to init unknown hash");
 }
 
-static void git_hash_unknown_clone(git_hash_ctx *dst, const git_hash_ctx *src)
+static void git_hash_unknown_clone(git_hash_ctx *UNUSED(dst),
+				   const git_hash_ctx *UNUSED(src))
 {
 	BUG("trying to clone unknown hash");
 }
 
-static void git_hash_unknown_update(git_hash_ctx *ctx, const void *data, size_t len)
+static void git_hash_unknown_update(git_hash_ctx *UNUSED(ctx),
+				    const void *UNUSED(data),
+				    size_t UNUSED(len))
 {
 	BUG("trying to update unknown hash");
 }
 
-static void git_hash_unknown_final(unsigned char *hash, git_hash_ctx *ctx)
+static void git_hash_unknown_final(unsigned char *UNUSED(hash),
+				   git_hash_ctx *UNUSED(ctx))
 {
 	BUG("trying to finalize unknown hash");
 }
