4ef36e228671bdbfc21f75be4b602b90c8af20d8
Jeff King <peff@peff.net>
2019-05-18 03:22:05 -0400

    transport_anonymize_url(): support retaining username
    When we anonymize URLs to show in messages, we strip out both the
    username and password (if any). But there are also contexts where we
    should strip out the password (to avoid leaking it) but retain the
    username.
    
    Let's generalize transport_anonymize_url() to support both cases. We'll
    give it a new name since the password-only mode isn't really
    "anonymizing", but keep the ld name as a synonym to avoid disrupting
    existing callers.
    
    Note that there are actually three places we parse URLs, and this
    functionality _could_ go into any of them:
    
      - transport_anonymize_url(), which we modify here
    
      - the urlmatch.c code parses a URL into its constituent parts, from
        which we could easily remove the elements we want to drop and
        re-format it as a single URL. But its parsing also normalizes
        elements (e.g., downcasing hostnames).  This isn't wrong, but it's
        more friendly if we can leave the rest of the URL untouched.
    
      - credential_form_url() parses a URL and decodes the specific
        elements, but it's hard to convert it back into a regular URL. It
        treats "host:port" as a single unit, meaning it needs to be
        re-encoded specially (since a colon would otherwise end
        percent-encoded).
    
    Since transport_anonymize_url() seemed closest to what we want here, I
    used that as the base.
    
    Signed-off-by: Jeff King <peff@peff.net>

--- transport.h
+++ transport.h
@@ -320,9 +320,18 @@ int transport_fetch_refs(struct transport *transport, struct ref *refs);
 void transport_unlock_pack(struct transport *transport, unsigned int flags);
 
 int transport_disconnect(struct transport *transport);
-char *transport_anonymize_url(const char *url);
 void transport_take_over(struct transport *transport,
 			 struct child_process *child);
+
+/*
+ * Strip password and optionally username from a URL and return
+ * it in a newly allocated string (even if nothing was stripped).
+ */
+char *transport_strip_url(const char *url, int strip_username);
+static inline char *transport_anonymize_url(const char *url)
+{
+	return transport_strip_url(url, 1);
+}
 
 int transport_connect(struct transport *transport, const char *name,
 		      const char *exec, int fd[2]);
