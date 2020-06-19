295f2b0aa194945ef04b3f2882b05989b0ae7438
Jeff King <peff@peff.net>
2020-06-19 14:32:14 -0400

    ident: allow negative timestamps in commit/tag ident lines
    When parsing a commit object, we don't allow a "-" before the digits of
    the timestamp. Let's loosen this, which allows us to show the correct
    date for objects before 1970. It's still somewhat awkward to _create_
    them, so we'll use hash-object to test some manually-created objects.
    
    The callers of split_ident_line() don't need updated. They already use
    parse_timestamp(), which correctly handles these negative dates.
    Likewise, fsck feeds the date line directly to parse_timestamp(), which
    is happy with a negative value.
    
    On the formatting side, we feed most formats to gmtime() or localtime(),
    and those handle negative timestamps just fine. Unix times are formatted
    using our PRItime abstraction, so that also shows negative times fine.
    We'll also test both author and local time zone display, since that
    should exercise more of our internal zone-munging functions.
    
    Signed-off-by: Jeff King <peff@peff.net>

---
+++ t/t1200-negative-timestamps.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+test_description='handling of timestamps before 1970'
+. ./test-lib.sh
+
+test_expect_success 'create a commit with a negative timestamp' '
+	tree=$(git hash-object -w -t tree --stdin </dev/null) &&
+	commit=$(
+		git hash-object -w -t commit --stdin <<-EOF
+		tree $tree
+		author $GIT_AUTHOR_NAME <$GIT_AUTHOR_EMAIL> -700000 +0100
+		committer $GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL> -700000 +0100
+
+		subject
+		EOF
+	)
+'
+
+test_expect_success 'fsck does not complain about negative timestamps' '
+	git fsck
+'
+
+test_expect_success 'show timestamp as unix date' '
+	git log --date=unix --format=%ad -1 $commit >actual &&
+	echo "-700000" >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success 'show timestamp in original zone' '
+	git log --date=iso --format=%ad -1 $commit >actual &&
+	echo "1969-12-23 22:33:20 +0100" >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success 'show timestamp in EST5' '
+	TZ=EST5 git log --date=iso-local --format=%ad -1 $commit >actual &&
+	echo "1969-12-23 16:33:20 -0500" >expect &&
+	test_cmp expect actual
+'
+
+test_done
