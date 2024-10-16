szeder/all-git-forks

receive-pack: pass receive.warnobjectsize to index-pack
This plumbs the config option through to index-pack, where
it will actually take effect. Our tests install a
pre-receive hook which parses the large-object log from
index-pack and acts on it.

[this should probably be called softObjectSizeLimit, and
there should probably be a corresponding "hard"]

builtin/receive-pack.c
@@ -48,6 +48,7 @@ static int advertise_atomic_push = 1;
 static int advertise_push_options;
 static int unpack_limit = 100;
 static off_t max_input_size;
+static off_t warn_object_size;
 static int report_status;
 static int use_sideband;
 static int use_atomic;
@@ -218,6 +219,11 @@ static int receive_pack_config(const char *var, const char *value, void *cb)
 
 	if (strcmp(var, "receive.maxinputsize") == 0) {
 		max_input_size = git_config_int64(var, value);
+		return 0;
+	}
+
+	if (strcmp(var, "receive.warnobjectsize") == 0) {
+		warn_object_size = git_config_ulong(var, value);
 		return 0;
 	}
 
@@ -1716,6 +1722,9 @@ static const char *unpack(int err_fd, struct shallow_info *si)
 		if (max_input_size)
 			argv_array_pushf(&child.args, "--max-input-size=%"PRIuMAX,
 				(uintmax_t)max_input_size);
+		if (warn_object_size)
+			argv_array_pushf(&child.args, "--warn-object-size=%"PRIuMAX,
+				(uintmax_t)warn_object_size);
 		child.out = -1;
 		child.err = err_fd;
 		child.git_cmd = 1;

peff/git

Signed-off-by: Jeff King <peff@peff.net>

builtin/receive-pack.c
@@ -68,6 +68,7 @@ static int advertise_push_options;
 static int advertise_sid;
 static int unpack_limit = 100;
 static off_t max_input_size;
+static off_t warn_object_size;
 static int report_status;
 static int report_status_v2;
 static int use_sideband;
@@ -267,6 +268,11 @@ static int receive_pack_config(const char *var, const char *value,
 
 	if (strcmp(var, "transfer.advertisesid") == 0) {
 		advertise_sid = git_config_bool(var, value);
+		return 0;
+	}
+
+	if (strcmp(var, "receive.warnobjectsize") == 0) {
+		warn_object_size = git_config_ulong(var, value, ctx->kvi);
 		return 0;
 	}
 
@@ -2295,6 +2301,9 @@ static const char *unpack(int err_fd, struct shallow_info *si)
 		if (max_input_size)
 			strvec_pushf(&child.args, "--max-input-size=%"PRIuMAX,
 				     (uintmax_t)max_input_size);
+		if (warn_object_size)
+			strvec_pushf(&child.args, "--warn-object-size=%"PRIuMAX,
+				     (uintmax_t)warn_object_size);
 		child.out = -1;
 		child.err = err_fd;
 		child.git_cmd = 1;