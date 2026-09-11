szeder/all-git-forks

http: refactor http_request function
This function takes a flag to indicate where the output
should go (either to a file or to a strbuf). This flag is
mostly used to set the callback function we hand to curl.

This isn't very flexible for adding new output types.
Instead, let's just let callers pass in the callback
function directly. This results in shorter, more readable,
and more flexible code.

The only other thing the flag was used for was to set a
"Range" header when we already have a partial file (by using
the results of ftell). This patch also adds an "offset"
parameter, which can be used by callers to specify this
feature separately (which is also more flexible, as non-FILE
callers can now resume partial transfers).

http.c
@@ -1600,8 +1600,8 @@ static void http_opt_request_remainder(CURL *curl, off_t pos)
 #define HTTP_REQUEST_STRBUF	0
 #define HTTP_REQUEST_FILE	1
 
-static int http_request(const char *url,
-			void *result, int target,
+static int http_request(const char *url, curl_write_callback cb,
+			void *result, long offset,
 			const struct http_get_options *options)
 {
 	struct active_request_slot *slot;
@@ -1619,17 +1619,11 @@ static int http_request(const char *url,
 	} else {
 		curl_easy_setopt(slot->curl, CURLOPT_NOBODY, 0);
 		curl_easy_setopt(slot->curl, CURLOPT_FILE, result);
-
-		if (target == HTTP_REQUEST_FILE) {
-			off_t posn = ftello(result);
-			curl_easy_setopt(slot->curl, CURLOPT_WRITEFUNCTION,
-					 fwrite);
-			if (posn > 0)
-				http_opt_request_remainder(slot->curl, posn);
-		} else
-			curl_easy_setopt(slot->curl, CURLOPT_WRITEFUNCTION,
-					 fwrite_buffer);
+		curl_easy_setopt(slot->curl, CURLOPT_WRITEFUNCTION, cb);
 	}
+
+	if (offset > 0)
+		http_opt_request_remainder(slot->curl, offset);
 
 	accept_language = get_accept_language();
 
@@ -1723,11 +1717,11 @@ static int update_url_from_redirect(struct strbuf *base,
 	return 1;
 }
 
-static int http_request_reauth(const char *url,
-			       void *result, int target,
+static int http_request_reauth(const char *url, curl_write_callback cb,
+			       void *result, unsigned long offset,
 			       struct http_get_options *options)
 {
-	int ret = http_request(url, result, target, options);
+	int ret = http_request(url, cb, result, offset, options);
 
 	if (options && options->effective_url && options->base_url) {
 		if (update_url_from_redirect(options->base_url,
@@ -1747,25 +1741,23 @@ static int http_request_reauth(const char *url,
 	 * the strbuf case, but that is enough to satisfy current callers.
 	 */
 	if (options && options->keep_error) {
-		switch (target) {
-		case HTTP_REQUEST_STRBUF:
+		/* XXX gross */
+		if (cb == fwrite_buffer)
 			strbuf_reset(result);
-			break;
-		default:
+		else
 			die("BUG: HTTP_KEEP_ERROR is only supported with strbufs");
-		}
 	}
 
 	credential_fill(&http_auth);
 
-	return http_request(url, result, target, options);
+	return http_request(url, cb, result, offset, options);
 }
 
 int http_get_strbuf(const char *url,
 		    struct strbuf *result,
 		    struct http_get_options *options)
 {
-	return http_request_reauth(url, result, HTTP_REQUEST_STRBUF, options);
+	return http_request_reauth(url, fwrite_buffer, result, 0, options);
 }
 
 /*
@@ -1789,7 +1781,7 @@ static int http_get_file(const char *url, const char *filename,
 		goto cleanup;
 	}
 
-	ret = http_request_reauth(url, result, HTTP_REQUEST_FILE, options);
+	ret = http_request_reauth(url, NULL, result, ftell(result), options);
 	fclose(result);
 
 	if (ret == HTTP_OK && finalize_object_file(tmpfile.buf, filename))

peff/git

Signed-off-by: Jeff King <peff@peff.net>

http.c
@@ -2225,8 +2225,8 @@ static void http_opt_request_remainder(CURL *curl, off_t pos)
 #define HTTP_REQUEST_STRBUF	0
 #define HTTP_REQUEST_FILE	1
 
-static int http_request(const char *url,
-			void *result, int target,
+static int http_request(const char *url, curl_write_callback cb,
+			void *result, long offset,
 			struct http_get_options *options)
 {
 	struct active_request_slot *slot;
@@ -2244,19 +2244,13 @@ static int http_request(const char *url,
 	} else {
 		curl_easy_setopt(slot->curl, CURLOPT_NOBODY, 0L);
 		curl_easy_setopt(slot->curl, CURLOPT_WRITEDATA, result);
-
-		if (target == HTTP_REQUEST_FILE) {
-			off_t posn = ftello(result);
-			curl_easy_setopt(slot->curl, CURLOPT_WRITEFUNCTION,
-					 fwrite);
-			if (posn > 0)
-				http_opt_request_remainder(slot->curl, posn);
-		} else
-			curl_easy_setopt(slot->curl, CURLOPT_WRITEFUNCTION,
-					 fwrite_buffer);
+		curl_easy_setopt(slot->curl, CURLOPT_WRITEFUNCTION, cb);
 	}
 
 	curl_easy_setopt(slot->curl, CURLOPT_HEADERFUNCTION, fwrite_wwwauth);
+
+	if (offset > 0)
+		http_opt_request_remainder(slot->curl, offset);
 
 	accept_language = http_get_accept_language_header();
 
@@ -2405,8 +2399,9 @@ static long handle_rate_limit_retry(long slot_retry_after)
 }
 
 static int http_request_recoverable(const char *url,
-			       void *result, int target,
-			       struct http_get_options *options)
+				    curl_write_callback cb,
+				    void *result, unsigned long offset,
+				    struct http_get_options *options)
 {
 	static struct http_get_options empty_opts;
 	int i = 3;
@@ -2419,7 +2414,7 @@ static int http_request_recoverable(const char *url,
 	if (always_auth_proactively())
 		credential_fill(the_repository, &http_auth, 1);
 
-	ret = http_request(url, result, target, options);
+	ret = http_request(url, cb, result, offset, options);
 
 	if (ret != HTTP_OK && ret != HTTP_REAUTH && ret != HTTP_RATE_LIMITED)
 		return ret;
@@ -2478,7 +2473,7 @@ static int http_request_recoverable(const char *url,
 			http_reauth_prepare(1);
 		}
 
-		ret = http_request(url, result, target, options);
+		ret = http_request(url, cb, result, offset, options);
 	}
 	if (ret == HTTP_RATE_LIMITED) {
 		trace2_data_string("http", the_repository,
@@ -2492,7 +2487,7 @@ int http_get_strbuf(const char *url,
 		    struct strbuf *result,
 		    struct http_get_options *options)
 {
-	return http_request_recoverable(url, result, HTTP_REQUEST_STRBUF, options);
+	return http_request_recoverable(url, fwrite_buffer, result, 0, options);
 }
 
 /*
@@ -2516,7 +2511,7 @@ int http_get_file(const char *url, const char *filename,
 		goto cleanup;
 	}
 
-	ret = http_request_recoverable(url, result, HTTP_REQUEST_FILE, options);
+	ret = http_request_recoverable(url, NULL, result, ftell(result), options);
 	fclose(result);
 
 	if (ret == HTTP_OK && finalize_object_file(the_repository, tmpfile.buf, filename))